5ae0f62d51de0393fde3e9bfd1e29da7860c2c8f
Vardan Khasikyan <vardan@estotyvilnius.com>
2026-06-19 13:08:15 +0400

    fixed shadows

--- Assets/Scripts/WheelControl.cs
+++ Assets/Scripts/WheelControl.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 
-// Simple wheel control script from the Unity tutorial: https://docs.unity3d.com/2021.3/Documentation/Manual/WheelColliderTutorial.html 
 public class WheelControl : MonoBehaviour {
   public Transform WheelModel;
   public bool Steerable;
@@ -8,16 +7,9 @@ public class WheelControl : MonoBehaviour {
 
    public WheelCollider WheelCollider;
 
-  Vector3 _position;
-  Quaternion _rotation;
-
-
-  // Update is called once per frame
-  void Update() {
-    // Get the Wheel collider's world pose values and
-    // use them to set the wheel model's position and rotation
-    WheelCollider.GetWorldPose(out _position, out _rotation);
-    WheelModel.transform.position = _position;
-    WheelModel.transform.rotation = _rotation;
+  private void Update() {
+    WheelCollider.GetWorldPose(out var position, out var rotation);
+    WheelModel.transform.position = position;
+    WheelModel.transform.rotation = rotation;
   }
 }
